20716266b4b90b1b208677b886a87553f623990b
ion <delion.dev@gmail.com>
2025-07-06 22:39:24 +0900

    chore: App.tsx에서 색상 변수, 경로 alias, svgr 플러그인 동작 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import "./App.css";
+import '@/App.css';
+import ReactLogo from '@/assets/react.svg?react';
 
 function App() {
   return (
-    <>
-      <div className="text-24"></div>
-    </>
+    <div className='flex flex-col justify-center items-center '>
+      <ReactLogo />
+      <div className='text-24 text-primary-50'>[testing] StageMate</div>
+    </div>
   );
 }
 
