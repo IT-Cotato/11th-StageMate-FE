253a44cbd2b83aced7e9b1131b745bf21f3a1e9e
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-09 13:50:48 +0900

    feat/#3 주간 캘린더 컴포넌트 기본 구조 생성

--- src/components/main/WeekCalendar.tsx
+++ src/components/main/WeekCalendar.tsx
@@ -1,10 +1,20 @@
-/**
- * 메인 페이지 - 주별 캘린더
- * 미완성
- */
+import {useState} from 'react';
+
+export interface WeekDate {
+  date: Date;
+  day: number;
+  dayName: string;
+  isToday: boolean;
+  isSelected: boolean;
+}
 
 export default function WeekCalendar() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [weekDates, setWeekDates] = useState<WeekDate[]>([]);
+
   return (
-    <div className='flex w-[570px] h-[263px] px-[38px] py-[22px] bg-white rounded-[20px] -mt-[20px] relative z-0 m-16'></div>
+    <div className='flex flex-col w-[570px] h-[263px] px-[38px] py-[22px] bg-white rounded-[20px] -mt-[20px] relative z-0 m-16 gap-[10px]'>
+      <span className='font-bold text'>전체 일정 달력</span>
+    </div>
   );
 }
