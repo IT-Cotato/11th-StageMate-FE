48623ad489f80f650c3c786083fd7a8d81e0301b
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-09 22:00:32 +0900

    feat/#3: 로그인 유도용 일정 없음 버튼 추가 및 스타일 적용

--- src/components/main/WeekCalendar.tsx
+++ src/components/main/WeekCalendar.tsx
@@ -43,21 +43,26 @@ export default function WeekCalendar() {
 
   return (
     <div className='flex flex-col w-[570px] h-[263px] px-[38px] py-[22px] bg-white rounded-[20px] -mt-[8px] relative z-0 m-16 gap-[10px]'>
-      <span className='font-bold text-[24px]'>전체 일정 달력</span>
-      <div className='flex justify-between w-full'>
-        {weekDates.map((day, idx) => (
-          <div
-            key={idx}
-            className={`flex flex-col gap-[12px] cursor-pointer transition-colors
+      <div className='flex flex-col gap-[30px]'>
+        <span className='font-bold text-[24px]'>전체 일정 달력</span>
+        <div className='flex justify-between w-full'>
+          {weekDates.map((day, idx) => (
+            <div
+              key={idx}
+              className={`flex flex-col gap-[12px] cursor-pointer transition-colors
         ${day.isToday ? ' text-primary-30' : 'text-gray-30'}`}
-            onClick={() => handleDateClick(day.date)}>
-            <span className='font-medium text-[32px]'>{day.day}</span>
-            <span className='text-center font-normal text-[20px]'>
-              {day.dayName}
-            </span>
-          </div>
-        ))}
+              onClick={() => handleDateClick(day.date)}>
+              <span className='font-medium text-[32px]'>{day.day}</span>
+              <span className='text-center font-normal text-[20px]'>
+                {day.dayName}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
+      <button className='px-[142px] py-[5.5px] text-white font-bold text-[15px] bg-secondary-50 cursor-pointer'>
+        일정이 없습니다.로그인해주세요.
+      </button>
     </div>
   );
 }
