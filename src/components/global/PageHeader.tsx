ea43e70a616a44382399f23f5a71de5f482367ef
ion <delion.dev@gmail.com>
2025-07-12 08:51:19 +0900

    style/#7: PageHeader 컴포넌트 스타일 수정

--- src/components/global/PageHeader.tsx
+++ src/components/global/PageHeader.tsx
@@ -8,7 +8,7 @@ interface PageHeaderProps {
 const PageHeader = ({title}: PageHeaderProps) => {
   return (
     <div className='flex py-18 pr-19 pl-18 justify-center items-center shrink-0 bg-white'>
-      <div className='flex justify-between h-50 items-center'>
+      <div className='flex w-full justify-between items-center h-50'>
         <ChevronLeft className='w-50 h-50 aspect-square' />
         <p className='text-[#141313] text-2xl leading-[140%]'>{title}</p>
         <div className='flex justify-center items-center w-50 h-50 py-12 pr-[11.9px] pl-[10.3px]'>
