de0690cbfca4eab9672c65014b3fd092210d2829
ion <delion.dev@gmail.com>
2025-07-12 08:53:08 +0900

    feat/#7: ButtonStroke 공통 버튼 컴포넌트 구현

---
+++ src/components/global/ButtonStroke.tsx
@@ -0,0 +1,16 @@
+interface ButtonStrokeProps {
+  text: string;
+  onClick: () => void;
+}
+
+const ButtonStroke = ({text, onClick}: ButtonStrokeProps) => {
+  return (
+    <div
+      onClick={onClick}
+      className='flex h-60 w-full py-13 justify-center items-center gap-10 self-stretch rounded-[14px] border-1 border-solid border-primary text-primary text-center text-2xl font-bold leading-[140%]'>
+      {text}
+    </div>
+  );
+};
+
+export default ButtonStroke;
