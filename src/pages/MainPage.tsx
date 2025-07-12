e30285cee7106103b736673e4dcd826d7dcc0562
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-13 01:15:11 +0900

    chore/#13: mock schedule 데이터 분리

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,42 +3,14 @@ import Header from '@/components/main/Header';
 import WeekCalendar from '@/components/main/WeekCalendar';
 import type {Schedule} from '@/types/schedule';
 import Magazine from '@/components/main/Magazine';
+import {mockSchedules} from '@/mocks/mockSchedules';
 
 export default function MainPage() {
   /** mock user data */
   const isLoggedIn = true;
   const username = '민아';
 
-  const [schedules, setSchedules] = useState<Schedule[]>([
-    {
-      id: '0',
-      category: '연극',
-      title: '<어쩌구 연극> 3차 티켓팅',
-      isLike: false,
-      date: new Date('2025-07-10'),
-    },
-    {
-      id: '1',
-      category: '연극',
-      title: '<어쩌구 연극> 3차 티켓팅',
-      isLike: true,
-      date: new Date('2025-07-10'),
-    },
-    {
-      id: '2',
-      category: '콘서트',
-      title: '<어쩌구 콘서트> 3차 티켓팅',
-      isLike: true,
-      date: new Date('2025-07-11'),
-    },
-    {
-      id: '3',
-      category: '뮤지컬',
-      title: '<어쩌구 뮤지컬> 3차 티켓팅',
-      isLike: false,
-      date: new Date('2025-07-12'),
-    },
-  ]);
+  const [schedules, setSchedules] = useState<Schedule[]>(mockSchedules);
 
   const handleLikeClick = (clicked: Schedule) => {
     setSchedules((prev) =>
