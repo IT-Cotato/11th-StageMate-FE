08e6860e2a9b7271ad1395138877a0a6f3e873bb
DandelionQZ <delion.dev@gmail.com>
2025-07-14 22:40:59 +0900

    feat/#12: AccordionItem 컴포넌트 추가

---
+++ src/components/ui/accordion/AccordionItem.tsx
@@ -0,0 +1,42 @@
+import ChevronDown from '@/assets/chevrons/chevron-down.svg?react';
+import CustomCheckbox from '../checkbox/CustomCheckbox';
+import {useState} from 'react';
+
+interface AccordianItemProps {
+  checked: boolean;
+  title: string;
+  children: React.ReactNode;
+  onChange: () => void;
+}
+
+const AccordianItem = ({
+  title,
+  children,
+  onChange,
+  checked = false,
+}: AccordianItemProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className='flex flex-col items-start self-stretch'>
+      <div className='p-10 self-stretch'>
+        <div className='grow flex items-start gap-17 h-37'>
+          <CustomCheckbox checked={checked} onChange={onChange} />
+          <div
+            className='grow flex pr-7 cursor-pointer'
+            onClick={() => setIsOpen((isOpen) => !isOpen)}>
+            <p className='text-[22px] leading-[140%] text-[#141313] grow select-none'>
+              {title}
+            </p>
+            <ChevronDown
+              className={`w-30 h-30 aspect-square ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+            />
+          </div>
+        </div>
+      </div>
+      {isOpen && children}
+    </div>
+  );
+};
+
+export default AccordianItem;
