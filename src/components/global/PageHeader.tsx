93f5750d948ef69b80cf5e30668b6ffd3cb60fdb
DandelionQZ <delion.dev@gmail.com>
2025-07-17 01:45:21 +0900

    style/#16: PageHeader 컴포넌트의 디자인 수정

--- src/components/global/PageHeader.tsx
+++ src/components/global/PageHeader.tsx
@@ -7,20 +7,24 @@ interface PageHeaderProps {
 
 const PageHeader = ({title}: PageHeaderProps) => {
   return (
-    <div className='flex py-18 pr-19 pl-18 justify-center items-center shrink-0 bg-white'>
-      <div className='flex w-full justify-between items-center h-50'>
+    <div className='flex flex-col pt-23 pr-10 pl-6 justify-end items-center shrink-0 bg-white gap-20'>
+      <div className='flex w-full justify-between items-center h-40'>
         <ChevronLeft
           onClick={() => console.log('PageHeader 왼쪽 화살표 클릭')}
-          className='w-50 h-50 aspect-square'
+          className='w-40 h-40 aspect-square'
         />
         <p className='text-[#141313] text-2xl leading-[140%]'>{title}</p>
-        <div className='flex justify-center items-center w-50 h-50 py-12 pr-[11.9px] pl-[10.3px]'>
+        <div className='flex justify-center items-center w-40 h-40 py-[9.6px] pl-[8.23px] pr-[9.48px]'>
           <HomeOutline
             onClick={() => console.log('PageHeader 오른쪽 홈버튼 클릭')}
             className='w-full h-full'
           />
         </div>
       </div>
+      <div
+        className='w-full h-3 shrink-0 
+        bg-linear-[90deg,var(--color-white)_0%,var(--color-primary-2)_31.73%,var(--color-primary-2)_75.48%,var(--color-white)_100%]'
+      />
     </div>
   );
 };
