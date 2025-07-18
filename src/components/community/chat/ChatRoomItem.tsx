a628e3227e08433d1a55666ac43be1de366f2673
kimminna <kmina121777@naver.com>
2025-07-19 01:30:59 +0900

    feat/#20: 실시간 채팅방 리스트/아이템 컴포넌트 구현

---
+++ src/components/community/chat/ChatRoomItem.tsx
@@ -0,0 +1,23 @@
+import Wifi from '@/assets/community/live-chat-wifi.svg?react';
+import Door from '@/assets/community/live-chat-door.svg?react';
+import type {ChatRoom} from '@/types/community';
+
+interface ChatRoomItemProps {
+  room: ChatRoom;
+}
+const ChatRoomItem = ({room}: ChatRoomItemProps) => {
+  return (
+    <li className='flex w-full px-14 py-9 h-48 bg-[#ffffff] items-center'>
+      <Wifi className='mr-32' />
+      <div className='flex flex-row flex-1 justify-between'>
+        <div>{room.title}</div>
+        <div>
+          오픈 시간: {room.startTime} ~ {room.endTime}
+        </div>
+      </div>
+      <Door className='ml-11 cursor-pointer' />
+    </li>
+  );
+};
+
+export default ChatRoomItem;
---
+++ src/components/community/chat/ChatRoomList.tsx
@@ -0,0 +1,22 @@
+import Users from '@/assets/community/live-chat-users.svg?react';
+import {mockChatRooms} from '@/mocks/mockChatRooms';
+import LoadMoreButton from '../common/LoadMoreButton';
+import ChatRoomItem from './ChatRoomItem';
+const ChatRoomList = () => {
+  return (
+    <div className='flex flex-col gap-10'>
+      {/** title */}
+      <div className='flex flex-row gap-6 items-center'>
+        <Users />
+        <h1 className='text-xl font-bold '>실시간 채팅방</h1>
+      </div>
+      {/** 채팅방 리스트 렌더링 */}
+      {mockChatRooms.map((room) => (
+        <ChatRoomItem key={room.id} room={room} />
+      ))}
+      <LoadMoreButton />
+    </div>
+  );
+};
+
+export default ChatRoomList;
