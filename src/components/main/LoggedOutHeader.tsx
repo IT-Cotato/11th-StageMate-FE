32edf6370b8e6d1b19f7525a93b37f26c83ae9a4
ion <delion.dev@gmail.com>
2025-07-18 18:42:32 +0900

    refactor/#26: MainButton 컴포넌트에 onClick prop 추가 및 LoggedOutHeader에서 사용

--- src/components/main/LoggedOutHeader.tsx
+++ src/components/main/LoggedOutHeader.tsx
@@ -1,13 +1,24 @@
 import MainButton from './MainButton';
+import {useNavigate} from 'react-router-dom';
 
 const LoggedOutHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1 className='flex text-[20px] text-white font-bold mt-[6px] mb-[18px]'>
         StageMate에 오신 것을 환영합니다!
       </h1>
-      <MainButton text='로그인하기' fullWidth />
-      <MainButton text='회원가입하기' fullWidth />
+      <MainButton
+        onClick={() => navigate('/login')}
+        text='로그인하기'
+        fullWidth
+      />
+      <MainButton
+        onClick={() => navigate('/signup-condition')}
+        text='회원가입하기'
+        fullWidth
+      />
     </div>
   );
 };
--- src/components/main/MainButton.tsx
+++ src/components/main/MainButton.tsx
@@ -4,10 +4,16 @@
 
 interface MainButtonProps {
   text: string;
+  onClick: () => void;
   fullWidth?: boolean;
   fixedWidth?: number;
 }
-const MainButton = ({text, fullWidth, fixedWidth}: MainButtonProps) => {
+const MainButton = ({
+  text,
+  onClick,
+  fullWidth,
+  fixedWidth,
+}: MainButtonProps) => {
   const widthClass = fullWidth
     ? 'w-full'
     : fixedWidth
@@ -15,6 +21,7 @@ const MainButton = ({text, fullWidth, fixedWidth}: MainButtonProps) => {
       : 'w-full md:w-[288px]';
   return (
     <button
+      onClick={onClick}
       className={`flex justify-center items-center ${widthClass} px-[24px] py-[13px] h-40 rounded-[14px] border-[1px] border-white bg-white/20 text-[20px] text-white font-medium cursor-pointer mb-5`}
       style={fixedWidth ? {width: `${fixedWidth}px`} : {}}>
       {text}
