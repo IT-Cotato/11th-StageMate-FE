d6a813a64b8db20cc34b9f31e24271be0752c690
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-20 05:36:49 +0900

    fix/#31: 온보딩 문구 줄바꿈 안되는 문제 수정

--- src/components/modal/OnboardingModal/OnboardingWrapper.tsx
+++ src/components/modal/OnboardingModal/OnboardingWrapper.tsx
@@ -4,22 +4,46 @@ import useScrollLockWithRestore from '@/hooks/useScrollLockwithRestore';
 
 const onboardingPages = [
   {
-    message: '공연 달력 기능으로 전체 공연 일정을\n한눈에 볼 수 있어요',
+    message: (
+      <>
+        공연 달력 기능으로 전체 공연
+        <br />
+        일정을 한눈에 볼 수 있어요
+      </>
+    ),
     imageSrc: new URL('@/assets/onboarding-icons/calendar.svg', import.meta.url)
       .href,
   },
   {
-    message: '내가 본 공연을 편하게 아카이빙하고\n느낌을 기록해요',
+    message: (
+      <>
+        내가 본 공연을 편하게
+        <br />
+        아카이빙 하고 느낌을 기록해요
+      </>
+    ),
     imageSrc: new URL('@/assets/onboarding-icons/chat.svg', import.meta.url)
       .href,
   },
   {
-    message: '여러 사람들과 함께\n공연을 덕질하고 정보를 얻어요',
+    message: (
+      <>
+        여러 사람들과 함께
+        <br />
+        공연을 덕질하고 정보를 얻어요
+      </>
+    ),
     imageSrc: new URL('@/assets/onboarding-icons/notebook.svg', import.meta.url)
       .href,
   },
   {
-    message: '빠른 검색창으로 내가 찾고 싶은\n공연을 편하게 찾을 수 있어요',
+    message: (
+      <>
+        빠른 검색창으로 내가 찾고 싶은
+        <br />
+        공연을 편하게 찾을 수 있어요
+      </>
+    ),
     imageSrc: new URL('@/assets/onboarding-icons/search.svg', import.meta.url)
       .href,
     isLast: true,
@@ -41,7 +65,7 @@ export default function OnboardingWrapper({onDone}: {onDone: () => void}) {
   };
 
   return (
-    <div className='absolute inset-0 z-50 flex items-start justify-center pt-[24px] bg-black/30'>
+    <div className='absolute inset-0 z-50 flex items-start justify-center pt-[64px] bg-black/50'>
       <OnboardingModal
         message={message}
         imageSrc={imageSrc}
