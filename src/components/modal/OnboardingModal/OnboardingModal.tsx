a410de575ef998f43e63401bc5be13aa0dc86a74
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-20 05:39:38 +0900

    refactor/#31: 온보딩 컴포넌트 크기, 간격, 정렬 등 시각적 수정

--- src/components/modal/OnboardingModal/OnboardingModal.tsx
+++ src/components/modal/OnboardingModal/OnboardingModal.tsx
@@ -18,20 +18,20 @@ const OnboardingModal = ({
   isLastStep = false,
 }: OnboardingModalProps) => {
   return (
-    <div className='w-[500px] h-[700px] rounded-[20px] overflow-hidden bg-white flex flex-col'>
+    <div className='w-[300px] h-[520px] rounded-[20px] overflow-hidden bg-white flex flex-col'>
       {/* 이미지 영역 */}
-      <div className='pt-[101px] pb-[31px] flex justify-center bg-white'>
-        <img src={imageSrc} alt='Onboarding' className='w-[308px] h-[308px]' />
+      <div className='pt-[34px] pb-[60px] flex justify-center bg-white'>
+        <img src={imageSrc} alt='Onboarding' className='w-[211px] h-[214px]' />
       </div>
 
       {/* 보라색 영역 */}
-      <div className='h-[260px] rounded-b-[20px] bg-primary pt-[19px] pb-[41px] px-[37px] text-white flex flex-col gap-[88px]'>
-        <p className='text-[30px] font-bold text-center leading-tight'>
+      <div className='h-[212px] rounded-b-[20px] bg-primary pt-[40px] pb-[37px] px-[14px] text-white flex flex-col gap-[58px] justify-center'>
+        <p className='text-[20px] text-xl not-italic font-bold leading-[140%] flex justify-center'>
           {message}
         </p>
-        <div className='flex justify-between items-center'>
+        <div className='flex justify-between items-center px-[40px]'>
           {showPrev ? (
-            <button onClick={onPrev} className='text-[20px] font-medium'>
+            <button onClick={onPrev} className='text-[16px] font-medium'>
               이전
             </button>
           ) : (
@@ -39,14 +39,12 @@ const OnboardingModal = ({
           )}
 
           {isLastStep ? (
-            <button
-              onClick={onNext}
-              className='px-4 py-2 mt-2 rounded-lg border 1px border-white bg-white/19 text-[18px] font-semibold text-center'>
+            <button onClick={onNext} className='text-[16px] font-medium'>
               시작하기
             </button>
           ) : (
             showNext && (
-              <button onClick={onNext} className='text-[20px] font-medium'>
+              <button onClick={onNext} className='text-[16px] font-medium'>
                 다음
               </button>
             )
