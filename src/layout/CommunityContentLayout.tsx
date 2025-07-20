a591f32da65d6639a014e1232ff51e1541728131
kimminna <kmina121777@naver.com>
2025-07-20 09:53:04 +0900

    feat/#33: 게시글 전용 레이아웃 구현

---
+++ src/layout/CommunityContentLayout.tsx
@@ -0,0 +1,25 @@
+/**
+ * 커뮤니티 글 작성 페이지, 글 세부 내용 페이지의 레이아웃
+ */
+
+import ContentHeader from '@/components/community/content/ContentHeader';
+import ContentNavigationBar from '@/components/community/content/ContentNavigationBar';
+import {Outlet} from 'react-router-dom';
+
+const CommunityContentLayout = () => {
+  return (
+    <div className='w-full min-h-screen flex justify-center overflow-x-hidden '>
+      <div className='w-full max-w-[600px] pb-[90px]'>
+        <ContentHeader />
+        <main>
+          <Outlet />
+        </main>
+        <footer className='w-full max-w-[600px] fixed bottom-0 left-1/2 -translate-x-1/2'>
+          <ContentNavigationBar />
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default CommunityContentLayout;
