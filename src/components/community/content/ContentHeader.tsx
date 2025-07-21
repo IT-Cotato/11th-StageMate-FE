959da99bd2c1b6f9030686f0aef44514da7c8998
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-22 03:17:32 +0900

    refactor/#46: ContentHeader에서 Share 아이콘 제거

--- src/components/community/content/ContentHeader.tsx
+++ src/components/community/content/ContentHeader.tsx
@@ -1,5 +1,4 @@
 import ChevronLeft from '@/assets/chevrons/chevron-left.svg?react';
-import Share from '@/assets/community/share.svg?react';
 import {useNavigate} from 'react-router-dom';
 
 const ContentHeader = () => {
@@ -8,7 +7,6 @@ const ContentHeader = () => {
     <div className='flex flex-col w-full '>
       <div className='w-full flex flex-row justify-between sm:px-30 sm:py-15 px-15 py-[7.5px]'>
         <ChevronLeft onClick={() => navigate(-1)} />
-        <Share />
       </div>
       <hr className='w-full h-[1px] bg-primary opacity-15 border-0' />
     </div>
