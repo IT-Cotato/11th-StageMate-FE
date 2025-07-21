792fb37a87049e1660a49cc11b780aa668900b3c
kimminna <kmina121777@naver.com>
2025-07-21 21:18:19 +0900

    style/#39: 모바일 대응을 위한 하단 네비게이션 바 및 아이콘 크기 조절

--- src/components/global/MainNavigationBar.tsx
+++ src/components/global/MainNavigationBar.tsx
@@ -4,12 +4,12 @@ import Archive from '@/assets/nav-icons/archive.svg?react';
 import Community from '@/assets/nav-icons/community.svg?react';
 import Settings from '@/assets/nav-icons/settings.svg?react';
 
-const NavigationBar = () => {
+const MainNavigationBar = () => {
   const location = useLocation();
   const currentPath = location.pathname;
 
   const getLinkClass = (path: string) =>
-    `w-[150px] h-[90px] flex items-center justify-center ${
+    `w-[150px] sm:h-[60px] h-[45px] flex items-center justify-center ${
       currentPath === path ? 'bg-white' : ''
     }`;
 
@@ -17,21 +17,29 @@ const NavigationBar = () => {
     currentPath === path ? 'text-primary' : 'text-[#918F9D]';
 
   return (
-    <div className='w-full max-w-full flex flex-row items-center bg-black'>
+    <div className='w-full max-w-full sm:h-[60px] h-[45px] flex flex-row items-center bg-black'>
       <Link to='/' className={getLinkClass('/')}>
-        <Home className={getIconClass('/')} />
+        <Home
+          className={`${getIconClass('/')} w-[18px] h-[18px] sm:w-[25px] sm:h-[25px]`}
+        />
       </Link>
       <Link to='/archive' className={getLinkClass('/archive')}>
-        <Archive className={getIconClass('/archive')} />
+        <Archive
+          className={`${getIconClass('/archive')} w-[18px] h-[18px] sm:w-[25px] sm:h-[25px]`}
+        />
       </Link>
       <Link to='/community' className={getLinkClass('/community')}>
-        <Community className={getIconClass('/community')} />
+        <Community
+          className={`${getIconClass('/community')} w-[18px] h-[18px] sm:w-[25px] sm:h-[25px]`}
+        />
       </Link>
       <Link to='/settings' className={getLinkClass('/settings')}>
-        <Settings className={getIconClass('/settings')} />
+        <Settings
+          className={`${getIconClass('/settings')} w-[18px] h-[18px] sm:w-[25px] sm:h-[25px]`}
+        />
       </Link>
     </div>
   );
 };
 
-export default NavigationBar;
+export default MainNavigationBar;
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,15 +1,15 @@
-import NavigationBar from '@/components/NavigationBar';
+import MainNavigationBar from '@/components/global/MainNavigationBar';
 import {Outlet} from 'react-router-dom';
 
 const MainLayout = () => {
   return (
     <div className='w-full min-h-screen flex justify-center overflow-x-hidden'>
-      <div className='w-full max-w-[600px] pb-[90px]'>
+      <div className='w-full max-w-[600px] sm:pb-60 pb-44'>
         <Outlet />
       </div>
 
       <footer className='w-full max-w-[600px] fixed bottom-0 left-1/2 -translate-x-1/2'>
-        <NavigationBar />
+        <MainNavigationBar />
       </footer>
     </div>
   );
