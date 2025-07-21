f686ac1806f6830aedd6a254d9286050510e937c
DandelionQZ <delion.dev@gmail.com>
2025-07-22 03:29:02 +0900

    feat/#42: 정렬 드롭다운 구현

--- src/pages/community/MagazineListPage.tsx
+++ src/pages/community/MagazineListPage.tsx
@@ -1,7 +1,56 @@
+import ChevronLeft from '@/assets/chevrons/chevron-left.svg?react';
+import {MagazineOrder} from '@/constant';
+import {useState} from 'react';
+
 const MagazineListPage = () => {
+  const [sortBy, setSortBy] = useState(0);
+  const [isOrderOpen, setIsOrderOpen] = useState(false);
+
+  const handleOrderClick = (id: number, text: string) => {
+    // todo : 백엔드 api 연결
+    console.log(text, ' 으로 api 다시 호출');
+    setSortBy(id);
+    setIsOrderOpen(false);
+  };
+
   return (
-    <div>
+    <div className='relative'>
+      {/* 정렬 선택 */}
+      <div className='flex flex-col justify-start items-end absolute top-0 right-15'>
+        <div className='flex flex-col items-start w-93 gap-1'>
+          <div
+            onClick={() => setIsOrderOpen((prev) => !prev)}
+            className='flex justify-between items-start self-stretch'>
+            <p className='text-primary font-roboto text-[15px] font-bold leading-[140%]'>
+              {MagazineOrder[sortBy].text}
+            </p>
+            <ChevronLeft
+              className={`w-20 h-20 aspect-square ${isOrderOpen ? '-rotate-90' : 'rotate-90'}`}
+            />
+          </div>
+
+          {isOrderOpen && (
+            <div>
+              {MagazineOrder.filter((order) => order.id !== sortBy).map(
+                (order) => (
+                  <p
+                    key={order.id}
+                    onClick={() => handleOrderClick(order.id, order.text)}
+                    className='text-primary font-roboto text-[15px] font-bold leading-[140%]'>
+                    {order.text}
+                  </p>
+                )
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* 리스트 */}
       <div></div>
+
+      {/* 페이지네이션 */}
+      {/* todo : 백엔드 api 연결  */}
     </div>
   );
 };
