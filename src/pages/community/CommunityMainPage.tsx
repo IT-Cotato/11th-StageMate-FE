4985ec9e8f0b72f6833d56ca19d76da6038a59b1
DandelionQZ <delion.dev@gmail.com>
2025-07-22 11:59:11 +0900

    fix/#42: 커뮤니티 게시판별 더보기 버튼 로직을 PostList 컴포넌트 내부에서 처리하도록 함

--- src/pages/community/CommunityMainPage.tsx
+++ src/pages/community/CommunityMainPage.tsx
@@ -15,7 +15,6 @@ const CommunityMainPage = () => {
         icon={<Rocket />}
         title='HOT 게시물'
         posts={mockPosts}
-        onLoadMoreClick={() => console.log('핫 게시물 더보기 버튼 클릭시 동작')}
         variant='hot'
       />
 
@@ -30,9 +29,6 @@ const CommunityMainPage = () => {
         icon={<MusicalNote />}
         title='일상 게시물'
         posts={mockPosts.filter((post) => post.category === '일상')}
-        onLoadMoreClick={() =>
-          console.log('일상 게시물 더보기 버튼 클릭시 동작')
-        }
         variant='daily'
       />
 
@@ -41,9 +37,6 @@ const CommunityMainPage = () => {
         icon={<School />}
         title='꿀팁 게시물'
         posts={mockPosts.filter((post) => post.category === '꿀팁')}
-        onLoadMoreClick={() =>
-          console.log('꿀팁 게시물 더보기 버튼 클릭시 동작')
-        }
         variant='tip'
       />
 
