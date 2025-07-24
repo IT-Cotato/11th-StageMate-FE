01cb6d5f8665950f0bbc7eccc3e434934f79f3de
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-24 12:19:16 +0900

    feat/#49: 커뮤니티 게시글 상세 페이지 댓글 기능 구현 및 스타일 정리

--- src/pages/community/CommunityPostPage.tsx
+++ src/pages/community/CommunityPostPage.tsx
@@ -1,11 +1,12 @@
 // 커뮤니티 게시글 상세 페이지
 import {useParams} from 'react-router-dom';
 import {mockPosts} from '@/mocks/mockPosts';
-import {mockComments} from '@/mocks/mockComments';
 import {useHorizontalScroll} from '@/hooks/useHorizontalScroll';
 import CommunityCategory from '@/components/community/common/CommunityCategory';
 import PostHeaderInfo from '@/components/community/post/PostHeaderInfo';
 import EllipsisVertical from '@/assets/ellipsis/ellipsis-vertical.svg?react';
+import PostImageList from '@/components/community/post/PostImageList';
+import PostComment from '@/components/community/post/PostComment';
 
 const CommunityPostPage = () => {
   const {postId} = useParams();
@@ -17,41 +18,29 @@ const CommunityPostPage = () => {
   }
 
   return (
-    <div className='flex flex-col gap-[21px] pt-[13px] pb-[40px] px-[33px]'>
-      <div className='flex justify-between items-center'>
-        <CommunityCategory category={post.category} />
-        <EllipsisVertical className='w-[26px] h-[26px]' />
+    <div className='flex flex-col gap-[21px] pt-[13px] pb-[40px]'>
+      <div className='flex flex-col gap-[21px] px-[17px]'>
+        <div className='flex justify-between items-center '>
+          <CommunityCategory category={post.category} />
+          <EllipsisVertical className='w-[26px] h-[26px]' />
+        </div>
+
+        <PostHeaderInfo
+          title={post.title}
+          nickname={post.nickname}
+          date={post.date}
+          viewCount={post.viewCount}
+          variant='detail'
+        />
+        <PostImageList
+          wrapperRef={listWrapperRef as React.RefObject<HTMLUListElement>}
+        />
+        <p className='font-normal text-[16px] leading-[110%] white-space: whitespace-pre-wrap break-words '>
+          {post.content}
+        </p>
       </div>
 
-      <PostHeaderInfo
-        title={post.title}
-        nickname={post.nickname}
-        date={post.date}
-        viewCount={post.viewCount}
-        variant='detail'
-      />
-      <ul ref={listWrapperRef} className='flex w-full overflow-x-auto gap-20'>
-        {post.imgUrls?.length
-          ? post.imgUrls.map((url, index) => (
-              <li
-                key={index}
-                className='min-w-[220px] h-[220px] bg-no-repeat bg-center bg-cover rounded-md shrink-0'
-                style={{backgroundImage: `url(${url})`}}
-              />
-            ))
-          : Array(4)
-              .fill(null)
-              .map((_, index) => (
-                <li
-                  key={index}
-                  className='min-w-[220px] h-[220px] bg-gray-1 rounded-md shrink-0 flex items-center justify-center text-sm text-black'>
-                  게시글 사진
-                </li>
-              ))}
-      </ul>
-      <p className='font-normal text-[16px] leading-[110%] white-space: whitespace-pre-wrap break-words'>
-        {post.content}
-      </p>
+      <PostComment />
     </div>
   );
 };
