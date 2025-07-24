363eda0cf2f435a2b9a673ae580d030400ac1e5c
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-24 17:27:02 +0900

    feat/#49: ContentHeader에 Share 아이콘 표시 여부 제어 prop 추가

--- src/components/community/content/ContentHeader.tsx
+++ src/components/community/content/ContentHeader.tsx
@@ -1,12 +1,18 @@
 import ChevronLeft from '@/assets/chevrons/chevron-left.svg?react';
+import Share from '@/assets/community/share.svg?react';
 import {useNavigate} from 'react-router-dom';
 
-const ContentHeader = () => {
+interface ContentHeaderProps {
+  showShare?: boolean;
+}
+
+const ContentHeader = ({showShare = true}: ContentHeaderProps) => {
   const navigate = useNavigate();
   return (
-    <div className='flex flex-col w-full '>
+    <div className='flex flex-col w-full bg-white'>
       <div className='w-full flex flex-row justify-between sm:px-30 sm:py-15 px-15 py-[7.5px]'>
         <ChevronLeft onClick={() => navigate(-1)} />
+        {showShare && <Share className='w-[50px] h-[50px]' />}
       </div>
       <hr className='w-full h-[1px] bg-primary opacity-15 border-0' />
     </div>
