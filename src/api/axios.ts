f1c1dd0a1bf4b4bdff145e17426f264951800333
ion <delion.dev@gmail.com>
2025-07-25 04:56:02 +0900

    feat/#43: axios 및 api 설정 파일 추가

---
+++ src/api/axios.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import {BASE_URL} from './urls';
+
+// 인증 필요 x
+export const publicAxios = axios.create({
+  baseURL: BASE_URL,
+});
+
+// 인증 필요 o
+export const privateAxios = axios.create({
+  baseURL: BASE_URL,
+});
+
+privateAxios.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+//privateAxios에  토큰이  없다면 로그인 페이지로 리다이렉트
+privateAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const {response} = error;
+    if (response?.status === 401) {
+      const token = localStorage.getItem('accessToken');
+      if (token) {
+        console.warn('토큰이 유효하지 않습니다. ');
+        localStorage.removeItem('accessToken');
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
---
+++ src/api/urls.ts
@@ -0,0 +1,7 @@
+import {API_URL} from './configs';
+
+export const BASE_URL = API_URL;
+
+export const ENDPOINT = {
+  // login
+};
