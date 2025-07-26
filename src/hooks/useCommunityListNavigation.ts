0b62d4dbd763fec3b188b43fdfa389ee15246e87
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-27 00:04:07 +0900

    feat/#49: 커뮤니티 관련 상세 페이지 이동 훅 추가

---
+++ src/hooks/useCommunityListNavigation.ts
@@ -0,0 +1,30 @@
+import {useNavigate} from 'react-router-dom';
+
+const useCommunityListNavigation = () => {
+  const navigate = useNavigate();
+
+  const goToCommunityCategory = (category: string) => {
+    navigate(`/community/${category}`);
+  };
+
+  const goToMagazineList = () => {
+    navigate('/magazine');
+  };
+
+  const goToShareList = () => {
+    navigate('/share');
+  };
+
+  const goToChatRoomList = () => {
+    navigate('/chat');
+  };
+
+  return {
+    goToCommunityCategory,
+    goToMagazineList,
+    goToShareList,
+    goToChatRoomList,
+  };
+};
+
+export default useCommunityListNavigation;
