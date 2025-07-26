11022278eff0783aea6b70e8f9613d6266490f4f
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-27 03:54:26 +0900

    feat/#49: 게시물 리스트 컴포넌트 훅 적용 및 페이지 연결

--- src/pages/community/CommunityMainPage.tsx
+++ src/pages/community/CommunityMainPage.tsx
@@ -6,8 +6,11 @@ import School from '@/assets/community/tip-post-school.svg?react';
 import Rocket from '@/assets/community/hot-post-rocket.svg?react';
 import MusicalNote from '@/assets/community/daily-post-musical-note.svg?react';
 import PlayMagazine from '@/components/community/magazine/PlayMagazine';
+import useCommunityListNavigation from '@/hooks/useCommunityListNavigation';
 
 const CommunityMainPage = () => {
+  const {goToCommunityCategory} = useCommunityListNavigation();
+
   return (
     <div className='flex flex-col gap-30 mb-12'>
       {/** hot 게시물 */}
@@ -16,10 +19,11 @@ const CommunityMainPage = () => {
         title='HOT 게시물'
         posts={mockPosts}
         variant='hot'
+        onClick={() => goToCommunityCategory('hot')}
       />
 
       {/** 나눔 거래 게시판 */}
-      <SharePostList />
+      <SharePostList variant='share' />
 
       {/** 실시간 채팅방 */}
       <ChatRoomList />
@@ -30,6 +34,7 @@ const CommunityMainPage = () => {
         title='일상 게시물'
         posts={mockPosts.filter((post) => post.category === '일상')}
         variant='daily'
+        onClick={() => goToCommunityCategory('daily')}
       />
 
       {/** 꿀팁 게시물 */}
@@ -38,6 +43,7 @@ const CommunityMainPage = () => {
         title='꿀팁 게시물'
         posts={mockPosts.filter((post) => post.category === '꿀팁')}
         variant='tip'
+        onClick={() => goToCommunityCategory('tip')}
       />
 
       {/** 공연 매거진 */}
