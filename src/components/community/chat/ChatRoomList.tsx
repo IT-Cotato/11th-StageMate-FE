b9f86908f06b4c08da213536a914930679c21dbb
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-27 00:06:05 +0900

    feat/#49: 채팅방 상세 페이지 이동용 네비게이션 훅 적용

--- src/components/community/chat/ChatRoomList.tsx
+++ src/components/community/chat/ChatRoomList.tsx
@@ -2,21 +2,30 @@ import Users from '@/assets/community/live-chat-users.svg?react';
 import {mockChatRooms} from '@/mocks/mockChatRooms';
 import LoadMoreButton from '../common/LoadMoreButton';
 import ChatRoomItem from './ChatRoomItem';
+import useCommunityNavigation from '@/hooks/useCommunityNavigation';
+import useCommunityListNavigation from '@/hooks/useCommunityListNavigation';
+
 const ChatRoomList = () => {
+  const {goToChatRoomDetail} = useCommunityNavigation();
+  const {goToChatRoomList} = useCommunityListNavigation();
+
   return (
     <div className='flex flex-col gap-10'>
-      {/** title */}
+      {/* title */}
       <div className='flex flex-row gap-6 items-center'>
         <Users />
-        <h1 className='text-xl font-bold '>실시간 채팅방</h1>
+        <h1 className='text-xl font-bold'>실시간 채팅방</h1>
       </div>
       {/** 채팅방 리스트 렌더링 */}
       {mockChatRooms.map((room) => (
-        <ChatRoomItem key={room.id} room={room} />
+        <ChatRoomItem
+          key={room.id}
+          room={room}
+          onClick={() => goToChatRoomDetail(room.id)}
+        />
       ))}
-      <LoadMoreButton
-        onClick={() => console.log('채팅방 리스트 페이지로 연결')}
-      />
+
+      <LoadMoreButton onClick={goToChatRoomList} />
     </div>
   );
 };
