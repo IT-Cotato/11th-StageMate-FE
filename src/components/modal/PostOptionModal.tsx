10f416c4dec78486b49d8330f81e7178c003744b
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-29 22:17:31 +0900

    feat/#49: 호버 시 chevron right 추가, onClose 추가

--- src/components/modal/PostOptionModal.tsx
+++ src/components/modal/PostOptionModal.tsx
@@ -3,6 +3,7 @@ import Ban from '@/assets/community/modal-icons/ban.svg?react';
 import Exclamation from '@/assets/community/modal-icons/exclamation.svg?react';
 import Pencil from '@/assets/community/modal-icons/pencil.svg?react';
 import Trash from '@/assets/community/modal-icons/trash.svg?react';
+import ChevronRight from '@/assets/chevrons/chevron-right.svg?react';
 
 interface PostOptionModalProps {
   showReply?: boolean;
@@ -10,48 +11,83 @@ interface PostOptionModalProps {
   showDelete?: boolean;
   showReport?: boolean;
   showBlock?: boolean;
+  onSelect: (type: 'delete' | 'report' | 'block') => void;
+  onClose: () => void;
 }
+
 const PostOptionModal = ({
   showReply,
   showEdit,
   showDelete,
   showReport,
   showBlock,
+  onSelect,
+  onClose,
 }: PostOptionModalProps) => {
   return (
-    <div className='flex flex-col bg-[#FFFFFF] divide-y divide-gray-1 py-4 rounded-[5px] z-50'>
+    <div className='flex flex-col bg-[#ffffff] py-4 rounded-[5px] z-50 divide-y divide-gray-1'>
       {showReply && (
-        <button className='flex items-center gap-7 px-5 cursor-pointer'>
-          <ArrowReply className='w-24 h-24' />
-          <span>답글 달기</span>
+        <button className='group flex items-center justify-between gap-7 px-5 py-2 cursor-pointer'>
+          <div className='flex items-center gap-7'>
+            <ArrowReply className='w-24 h-24' />
+            <span>답글 달기</span>
+          </div>
+          <ChevronRight className='w-16 h-16 opacity-0 group-hover:opacity-100 transition' />
         </button>
       )}
 
       {showEdit && (
-        <button className='flex items-center gap-7 px-5 cursor-pointer'>
-          <Pencil className='w-24 h-24' />
-          <span>수정</span>
+        <button className='group flex items-center justify-between gap-7 px-5 py-2 cursor-pointer'>
+          <div className='flex items-center gap-7'>
+            <Pencil className='w-24 h-24' />
+            <span>수정</span>
+          </div>
+          <ChevronRight className='w-16 h-16 opacity-0 group-hover:opacity-100 transition' />
         </button>
       )}
 
       {showDelete && (
-        <button className='flex items-center gap-7 px-5 cursor-pointer'>
-          <Trash className='w-24 h-24' />
-          <span>삭제</span>
+        <button
+          onClick={() => {
+            onSelect('delete');
+            onClose();
+          }}
+          className='group flex items-center justify-between gap-7 px-5 py-2 cursor-pointer'>
+          <div className='flex items-center gap-7'>
+            <Trash className='w-24 h-24' />
+            <span>삭제</span>
+          </div>
+          <ChevronRight className='w-16 h-16 opacity-0 group-hover:opacity-100 transition' />
         </button>
       )}
 
       {showReport && (
-        <button className='flex items-center gap-7 px-5 cursor-pointer'>
-          <Exclamation className='w-24 h-24' />
-          <span className='text-red'>신고</span>
+        <button
+          onClick={() => {
+            onSelect('report');
+            onClose();
+          }}
+          className='group flex items-center justify-between gap-7 px-5 py-2 cursor-pointer'>
+          <div className='flex items-center gap-7'>
+            <Exclamation className='w-24 h-24' />
+            <span className='text-red'>신고</span>
+          </div>
+          <ChevronRight className='w-16 h-16 opacity-0 group-hover:opacity-100 transition' />
         </button>
       )}
 
       {showBlock && (
-        <button className='flex items-center gap-7 px-5 cursor-pointer'>
-          <Ban className='w-24 h-24' />
-          <span className='text-red'>차단</span>
+        <button
+          onClick={() => {
+            onSelect('block');
+            onClose();
+          }}
+          className='group flex items-center justify-between gap-7 px-5 py-2 cursor-pointer'>
+          <div className='flex items-center gap-7'>
+            <Ban className='w-24 h-24' />
+            <span className='text-red'>차단</span>
+          </div>
+          <ChevronRight className='w-16 h-16 opacity-0 group-hover:opacity-100 transition' />
         </button>
       )}
     </div>
