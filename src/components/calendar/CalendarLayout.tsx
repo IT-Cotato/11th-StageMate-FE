f11c2d84cf7695e5001e5cbb89fe06d0ce051650
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-31 16:01:31 +0900

    feat/#69: CalendarLayout 컴포넌트 분리 및 커스텀 헤더 적용

---
+++ src/components/calendar/CalendarLayout.tsx
@@ -0,0 +1,68 @@
+import {Calendar} from 'react-big-calendar';
+import localizer from './localizer';
+import CustomDateHeader from './CustomDateHeader';
+import CustomWeekHeader from './CustomWeekHeader';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+export interface CalendarEvent {
+  id?: string | number;
+  title?: string;
+  start: Date;
+  end: Date;
+  imageUrl?: string;
+}
+
+type CalendarLayoutProps = {
+  events: CalendarEvent[];
+  onDateClick: (date: Date) => void;
+  onEventClick?: (event: CalendarEvent) => void;
+  currentDate: Date;
+  onNavigateDate?: (date: Date) => void;
+};
+
+const CalendarLayout = ({
+  events,
+  onDateClick,
+  onEventClick,
+  currentDate,
+  onNavigateDate,
+}: CalendarLayoutProps) => {
+  const handleNavigate = (date: Date) => {
+    onNavigateDate?.(date);
+  };
+
+  return (
+    <div className='w-full'>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        date={currentDate}
+        onNavigate={handleNavigate}
+        startAccessor='start'
+        endAccessor='end'
+        views={['month']}
+        defaultView='month'
+        toolbar={false}
+        style={{height: 600, width: '100%'}}
+        components={{
+          month: {
+            header: CustomWeekHeader,
+            dateHeader: (props) => (
+              <CustomDateHeader
+                {...props}
+                events={events}
+                onDateClick={onDateClick}
+                showImages={true}
+                localizer={localizer}
+              />
+            ),
+            event: () => null,
+          },
+        }}
+        onSelectEvent={onEventClick}
+      />
+    </div>
+  );
+};
+
+export default CalendarLayout;
