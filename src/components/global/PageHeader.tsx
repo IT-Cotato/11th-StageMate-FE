c0734ad7039d690ba807f9e8cb00d4b50f344c2a
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-08-04 20:46:36 +0900

    refactor/#81: PageHeader props 선택적 처리 및 하단 라인 유동화

--- src/components/global/PageHeader.tsx
+++ src/components/global/PageHeader.tsx
@@ -1,32 +1,50 @@
 import ChevronLeft from '@/assets/chevrons/chevron-left.svg?react';
 import HomeOutline from '@/assets/bottomNavigationBar/home-outline.svg?react';
 
-interface PageHeaderProps {
-  title: string;
-  onLeftClick: () => void;
-  onRightClick: () => void;
+export interface PageHeaderProps {
+  title?: string;
+  onLeftClick?: () => void;
+  onRightClick?: () => void;
+  showHomeIcon?: boolean;
+  showBottomLine?: boolean;
+  className?: string;
 }
 
-const PageHeader = ({title, onLeftClick, onRightClick}: PageHeaderProps) => {
+const PageHeader = ({
+  title,
+  onLeftClick,
+  onRightClick,
+  showHomeIcon = true,
+  showBottomLine = true,
+  className = '',
+}: PageHeaderProps) => {
   return (
-    <div className='flex flex-col pt-23 pr-10 pl-6 justify-end items-center shrink-0 bg-white gap-20'>
+    <div
+      className={`flex flex-col pr-10 pl-6 justify-end items-center shrink-0 bg-white gap-20 z-50 ${className}`}>
       <div className='flex w-full justify-between items-center h-40'>
         <ChevronLeft
           onClick={onLeftClick}
           className='w-40 h-40 aspect-square'
         />
         <p className='text-[#141313] text-2xl leading-[140%]'>{title}</p>
-        <div className='flex justify-center items-center w-40 h-40 py-[9.6px] pl-[8.23px] pr-[9.48px]'>
-          <HomeOutline
-            onClick={onRightClick}
-            className='w-[22.289px] h-[20.802px] shrink-0 aspect-[22.29/20.80]'
-          />
-        </div>
+        {showHomeIcon ? (
+          <div className='flex justify-center items-center w-40 h-40'>
+            <HomeOutline
+              onClick={onRightClick}
+              className='w-[22.289px] h-[20.802px]'
+            />
+          </div>
+        ) : (
+          <div className='w-40 h-40' />
+        )}
       </div>
-      <div
-        className='w-full h-3 shrink-0 
-        bg-linear-[90deg,var(--color-white)_0%,var(--color-primary-2)_31.73%,var(--color-primary-2)_75.48%,var(--color-white)_100%]'
-      />
+
+      {showBottomLine && (
+        <div
+          className='w-full h-3 shrink-0 
+    bg-[linear-gradient(90deg,var(--color-white)_0%,var(--color-primary-2)_31.73%,var(--color-primary-2)_75.48%,var(--color-white)_100%)]'
+        />
+      )}
     </div>
   );
 };
