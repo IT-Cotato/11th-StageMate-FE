1d598ace7c91ef2268a1498d6d263b6efc283b65
ion <delion.dev@gmail.com>
2025-08-05 18:08:22 +0900

    feat/#43: 일반 회원가입 임시유저키 발급 및 약관 동의 api 엔드포인트 작성

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -3,5 +3,7 @@ import {API_URL} from './configs';
 export const BASE_URL = API_URL;
 
 export const ENDPOINT = {
-  // login
+  // signup
+  AUTH_SIGNUP_TEMPUSERKEY: 'api/v1/auth/sign-up/tempUserKey',
+  AUTH_SIGNUP_AGREE: 'api/v1/auth/sign-up/agree',
 };
