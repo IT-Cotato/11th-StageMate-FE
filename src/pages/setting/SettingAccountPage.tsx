beb0df0daff0fb1edd96691822f54e6eed3c1ddc
DandelionQZ <delion.dev@gmail.com>
2025-08-06 16:55:12 +0900

    style/#82: css 스타일 수정
    텍스트 스타일 및 버튼 min-w 추가

--- src/pages/setting/SettingAccountPage.tsx
+++ src/pages/setting/SettingAccountPage.tsx
@@ -13,25 +13,35 @@ const SettingAccountPage = () => {
       {/* info */}
       <div className='mx-28 py-37 px-33 flex flex-col gap-26 bg-[#d9d9d9]'>
         <div className='border-b border-solid border-[#000]'>
-          <p>{accountInfo.id || '아이디'}</p>
+          <p className='text-[#000] text-lg font-semibold leading-[140%]'>
+            {accountInfo.id || '아이디'}
+          </p>
         </div>
         <div className='border-b border-solid border-[#000]'>
-          <p>{accountInfo.email || '이메일'}</p>
+          <p className='text-[#000] text-lg font-semibold leading-[140%]'>
+            {accountInfo.email || '이메일'}
+          </p>
         </div>
         <div className='border-b border-solid border-[#000]'>
-          <p>{accountInfo.name || '이름'}</p>
+          <p className='text-[#000] text-lg font-semibold leading-[140%]'>
+            {accountInfo.name || '이름'}
+          </p>
         </div>
         <div className='border-b border-solid border-[#000]'>
-          <p>{accountInfo.nickname || '닉네임'}</p>
+          <p className='text-[#000] text-lg font-semibold leading-[140%]'>
+            {accountInfo.nickname || '닉네임'}
+          </p>
         </div>
         <div className='border-b border-solid border-[#000]'>
-          <p>{accountInfo.birth || '생년월일'}</p>
+          <p className='text-[#000] text-lg font-semibold leading-[140%]'>
+            {accountInfo.birth || '생년월일'}
+          </p>
         </div>
       </div>
 
       {/* change pw */}
       <div className='flex justify-end'>
-        <button className='py-4 px-10 border border-solid border-primary bg-white_1 text-[15px] leading-[140%] text-primary'>
+        <button className='py-4 px-10 min-x-80 border border-solid border-primary bg-white_1 text-[15px] leading-[140%] text-primary'>
           비밀번호 변경
         </button>
       </div>
