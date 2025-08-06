c76065c4a045908edfe08321ad1b3a4c9dd05854
DandelionQZ <delion.dev@gmail.com>
2025-08-07 00:05:24 +0900

    feat/#88: AnnouncementDetailPage를 mockData를 활용해서 UI 구현

--- src/pages/setting/AnnouncementDetailPage.tsx
+++ src/pages/setting/AnnouncementDetailPage.tsx
@@ -1,6 +1,12 @@
 import BackButtonTitleHeader from '@/components/global/BackButtonTitleHeader';
+import {mockAnnouncementDetail} from '@/mocks/mockAnnouncementDetail';
+import {useParams} from 'react-router-dom';
 
 const AnnouncementDetailPage = () => {
+  const {id} = useParams();
+
+  const data = mockAnnouncementDetail[Number(id)];
+
   return (
     <div>
       {/* header */}
@@ -8,9 +14,16 @@ const AnnouncementDetailPage = () => {
 
       {/* announcement */}
       <div className='py-24 px-20 flex flex-col justify-start items-start gap-29'>
-        <h1></h1>
-        <h2></h2>
-        <pre></pre>
+        <h1 className='text-[#000] text-[34px] font-bold leading-[110%]'>
+          {data.title}
+        </h1>
+        <h2 className='text-[#000] text-sm leading-[110%]'>
+          작성 시간 {data.createdAt} | 조회수 {data.viewCount} | 작성자{' '}
+          {data.author}
+        </h2>
+        <p className='text-[#000] text-xl leading-[110%] whitespace-pre-line'>
+          {data.content}
+        </p>
       </div>
     </div>
   );
