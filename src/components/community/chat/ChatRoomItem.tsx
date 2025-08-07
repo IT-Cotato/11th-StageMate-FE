6e2cdbae777b7983260378b887c4874fb83907e1
kimminna <kmina121777@naver.com>
2025-08-08 00:31:24 +0900

    style/#94: 채팅방 디자인 수정 사항 반영 및 더 보기 컴포넌트 적용

--- src/components/community/chat/ChatRoomItem.tsx
+++ src/components/community/chat/ChatRoomItem.tsx
@@ -8,15 +8,15 @@ interface ChatRoomItemProps {
 }
 const ChatRoomItem = ({room, onClick}: ChatRoomItemProps) => {
   return (
-    <li className='flex w-full px-14 py-9 h-48 bg-[#ffffff] items-center'>
-      <Wifi className='mr-32' />
+    <li className='flex w-full px-14 py-9 h-48 bg-[#ffffff] items-center rounded-[10px] text-sm'>
+      <Wifi className='mr-32 w-20' />
       <div className='flex flex-row flex-1 justify-between cursor-pointer'>
         <div>{room.title}</div>
         <div>
           오픈 시간: {room.startTime} ~ {room.endTime}
         </div>
       </div>
-      <Door className='ml-11 cursor-pointer' onClick={onClick} />
+      <Door className='w-20 ml-11 cursor-pointer' onClick={onClick} />
     </li>
   );
 };
