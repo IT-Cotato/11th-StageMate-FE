80388029248287c7684e7919368ba4ffb5f1eec5
ion <delion.dev@gmail.com>
2025-08-09 19:31:30 +0900

    feat/#43: User 인터페이스를 타입으로 변경하고 SignupInfoType 및 FormState 추가

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,4 +1,43 @@
-export interface User {
-  id: string;
+export type User = {
+  userId: string;
+  email: string;
   name: string;
-}
+  nickname: string;
+  birth: string;
+  profileImageUrl: string | null;
+};
+
+export type VerifyCodeType = {
+  email: string;
+  code: string;
+};
+
+export type SignupInfoType = {
+  userId: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  name: string;
+  nickname: string;
+  birthdate: string;
+};
+
+type FieldState = {
+  value: string;
+  isValid: boolean;
+  isChecked?: boolean; // API 중복 확인을 실행했는지 여부
+  isAvailable?: boolean; // 중복/사용 가능 여부
+};
+
+export type FormState = {
+  id: FieldState;
+  pw: FieldState;
+  pwCheck: FieldState;
+  name: FieldState;
+  nickName: FieldState;
+  email: FieldState;
+  certificationNumber: FieldState;
+  year: FieldState;
+  month: FieldState;
+  day: FieldState;
+};
