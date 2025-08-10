30474916cc8b74a0ece265af68ffe575570433b3
DandelionQZ <delion.dev@gmail.com>
2025-08-10 21:15:46 +0900

    feat/#91: 비밀번호 변경 페이지 UI 구성 및 입력 필드 추가

--- src/pages/setting/ChangePassword.tsx
+++ src/pages/setting/ChangePassword.tsx
@@ -1,7 +1,72 @@
+import BackButtonTitleHeader from '@/components/global/BackButtonTitleHeader';
+import EyeOpen from '@/assets/eye-open.svg?react';
+
 const ChangePassword = () => {
   return (
     <div>
-      <div></div>
+      {/* header */}
+      <BackButtonTitleHeader title='비밀번호 변경' borderBottom />
+
+      {/* content */}
+      <div className='py-24 px-20 flex flex-col justify-start items-center gap-27'>
+        <div className='self-stretch flex flex-col gap-13'>
+          <h1 className='text-[#000] text-xl font-bold leading-24 tracking-[0.2px]'>
+            현재 비밀번호
+          </h1>
+          <div className='px-10'>
+            <div className='flex justify-between items-center py-9 px-12 rounded-[5px] border border-solid border-[#000] bg-white text-[#000] gap-10 overflow-hidden'>
+              <input
+                type='text'
+                minLength={8}
+                maxLength={16}
+                placeholder='기존 비밀번호를 입력해주세요'
+                className='grow placeholder:text-[#cdcdcd] placeholder:text-[18px] leading-24 tracking-[0.18px] focus:outline-0'
+              />
+              <EyeOpen className='hover:cursor-pointer shrink-0' />
+            </div>
+          </div>
+        </div>
+
+        <div className='self-stretch flex flex-col gap-13'>
+          <h1 className='text-[#000] text-xl font-bold leading-24 tracking-[0.2px]'>
+            새 비밀번호
+          </h1>
+          <div className='px-10'>
+            <div className='flex justify-between items-center py-9 px-12 rounded-[5px] border border-solid border-[#000] bg-white text-[#000] gap-10 overflow-hidden'>
+              <input
+                type='text'
+                minLength={8}
+                maxLength={16}
+                placeholder='새로운 비밀번호를 입력해주세요'
+                className='grow placeholder:text-[#cdcdcd] placeholder:text-[18px] leading-24 tracking-[0.18px] focus:outline-0'
+              />
+              <EyeOpen className='hover:cursor-pointer shrink-0' />
+            </div>
+          </div>
+        </div>
+
+        <div className='self-stretch flex flex-col gap-13'>
+          <h1 className='text-[#000] text-xl font-bold leading-24 tracking-[0.2px]'>
+            새 비밀번호 확인
+          </h1>
+          <div className='px-10'>
+            <div className='flex justify-between items-center py-9 px-12 rounded-[5px] border border-solid border-[#000] bg-white text-[#000] gap-10 overflow-hidden'>
+              <input
+                type='text'
+                minLength={8}
+                maxLength={16}
+                placeholder='새로운 비밀번호를 다시 입력해주세요'
+                className='grow placeholder:text-[#cdcdcd] placeholder:text-[18px] leading-24 tracking-[0.18px] focus:outline-0'
+              />
+              <EyeOpen className='hover:cursor-pointer shrink-0' />
+            </div>
+          </div>
+        </div>
+
+        <button className='self-stretch mx-10 py-11 rounded-[5px] border border-solid border-black bg-primary-2 text-white text-xl font-medium leading-20 hover:cursor-pointer'>
+          확인
+        </button>
+      </div>
     </div>
   );
 };
