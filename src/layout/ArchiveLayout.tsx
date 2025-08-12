20ef1dbdf7c7b3ea90d6b2dad1535fa7c66a0e92
kimminna <kmina121777@naver.com>
2025-08-12 23:46:54 +0900

    style/#63: 아카이빙 레이아웃 수정

--- src/layout/ArchiveLayout.tsx
+++ src/layout/ArchiveLayout.tsx
@@ -4,13 +4,13 @@ import {Outlet} from 'react-router-dom';
 
 const ArchiveLayout = () => {
   return (
-    <div className='w-full min-h-screen flex justify-center overflow-x-hidden'>
+    <div className='w-full min-h-screen flex justify-center overflow-x-hidden relative'>
       <div className='relative w-full max-w-[600px] pb-[90px]'>
-        <div className='fixed top-0 max-w-[600px] w-full bg-white z-50'>
+        <div className='fixed top-0 left-1/2 transform -translate-x-1/2 max-w-[600px] w-full bg-white z-50'>
           <CommunityMainHeader />
         </div>
 
-        <main className='pt-90 px-20'>
+        <main className='px-20'>
           <Outlet />
         </main>
 
