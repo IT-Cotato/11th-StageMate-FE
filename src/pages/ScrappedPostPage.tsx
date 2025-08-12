8107c6e3a4705e4af1d837be337cc39edcb8e7e5
DandelionQZ <delion.dev@gmail.com>
2025-08-12 13:42:05 +0900

    name, refactor/#92: 컴포넌트 명 및 경로 변경, 매거진 컴포넌트 정렬 방식 변경
    ScrappedPostList를 ScrappedPostPage로 이름을 변경.
    
    경로를 components/archive에서 page로 변경.

--- src/pages/ScrappedPostPage.tsx
+++ src/pages/ScrappedPostPage.tsx
@@ -1,13 +1,11 @@
 import {useEffect, useState} from 'react';
-import ChevronLeft from '@/assets/chevrons/chevron-left.svg?react';
-import {useNavigate} from 'react-router-dom';
 import Pagination from 'react-js-pagination';
 import {mockPosts} from '@/mocks/mockPosts';
-import PostListItem from '../community/post/PostListItem';
+import PostListItem from '@/components/community/post/PostListItem';
+import BackButtonTitleHeader from '@/components/global/BackButtonTitleHeader';
 const ITEMS_PER_PAGE = 9;
 
-const ScrappedPostList = () => {
-  const navigate = useNavigate();
+const ScrappedPostPage = () => {
   const scrappedPost = mockPosts.filter((post) => post.isScrapped === true);
   const [currentPage, setCurrentPage] = useState(1);
   const totalItemsCount = scrappedPost.length;
@@ -21,15 +19,9 @@ const ScrappedPostList = () => {
     window.scrollTo({top: 0, behavior: 'smooth'});
   }, []);
   return (
-    <div className='flex flex-col px-20 gap-40'>
+    <div className='flex flex-col gap-40'>
       {/* 상단 */}
-      <div className='flex flex-row justify-between items-center mb-6'>
-        <ChevronLeft
-          className='w-50 h-50 cursor-pointer'
-          onClick={() => navigate(-1)}
-        />
-        <span className='text-xl font-extrabold'>스크랩한 글 모아보기</span>
-      </div>
+      <BackButtonTitleHeader title='스크랩한 글 모아보기' between />
 
       {/* 리스트 */}
       <ul className='flex flex-col gap-10'>
@@ -58,4 +50,4 @@ const ScrappedPostList = () => {
     </div>
   );
 };
-export default ScrappedPostList;
+export default ScrappedPostPage;
