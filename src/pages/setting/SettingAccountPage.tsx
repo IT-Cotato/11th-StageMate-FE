749e0f4f72ad5dd0a8f2a27546efd2f26cfe3a76
DandelionQZ <delion.dev@gmail.com>
2025-08-12 17:24:13 +0900

    feat/#109:  사용자 정보를 계정 설정 페이지에 동적으로 표시

--- src/pages/setting/SettingAccountPage.tsx
+++ src/pages/setting/SettingAccountPage.tsx
@@ -1,16 +1,9 @@
+import {useAuthStore} from '@/stores/authStore';
 import {useNavigate} from 'react-router-dom';
-
-// 임시 계정 정보
-const accountInfo = {
-  id: '아이디',
-  email: '이메일',
-  name: '이름',
-  nickname: '닉네임',
-  birth: '생년월일',
-};
 
 const SettingAccountPage = () => {
   const navigate = useNavigate();
+  const {user} = useAuthStore();
 
   return (
     <div className='flex flex-col gap-29'>
@@ -20,41 +13,31 @@ const SettingAccountPage = () => {
           <p className='text-[#000] text-lg font-semibold leading-[140%]'>
             아이디
           </p>
-          <p className='text-[#000] text-lg leading-[140%]'>
-            {accountInfo.id || '아이디'}
-          </p>
+          <p className='text-[#000] text-lg leading-[140%]'>{user?.userId}</p>
         </div>
         <div className='border-b border-solid border-[#000] flex items-center gap-12'>
           <p className='text-[#000] text-lg font-semibold leading-[140%]'>
             이메일
           </p>
-          <p className='text-[#000] text-lg leading-[140%]'>
-            {accountInfo.email || '이메일'}
-          </p>
+          <p className='text-[#000] text-lg leading-[140%]'>{user?.email}</p>
         </div>
         <div className='border-b border-solid border-[#000] flex items-center gap-12'>
           <p className='text-[#000] text-lg font-semibold leading-[140%]'>
             이름
           </p>
-          <p className='text-[#000] text-lg leading-[140%]'>
-            {accountInfo.name || '이름'}
-          </p>
+          <p className='text-[#000] text-lg leading-[140%]'>{user?.name}</p>
         </div>
         <div className='border-b border-solid border-[#000] flex items-center gap-12'>
           <p className='text-[#000] text-lg font-semibold leading-[140%]'>
             닉네임
           </p>
-          <p className='text-[#000] text-lg leading-[140%]'>
-            {accountInfo.nickname || '닉네임'}
-          </p>
+          <p className='text-[#000] text-lg leading-[140%]'>{user?.nickname}</p>
         </div>
         <div className='border-b border-solid border-[#000] flex items-center gap-12'>
           <p className='text-[#000] text-lg font-semibold leading-[140%]'>
             생년월일
           </p>
-          <p className='text-[#000] text-lg leading-[140%]'>
-            {accountInfo.birth || '생년월일'}
-          </p>
+          <p className='text-[#000] text-lg leading-[140%]'>{user?.birth}</p>
         </div>
       </div>
 
