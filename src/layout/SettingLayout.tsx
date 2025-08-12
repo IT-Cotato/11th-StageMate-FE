0ab14921f1d2257dcefefb8cb8cdaad2c51ceaf4
DandelionQZ <delion.dev@gmail.com>
2025-08-12 17:58:44 +0900

    feat/#109: 사용자 프로필 이미지 및 닉네임 동적 표시 기능 추가

--- src/layout/SettingLayout.tsx
+++ src/layout/SettingLayout.tsx
@@ -1,31 +1,37 @@
 import SettingTabBar from '@/components/setting/SettingTabBar';
+import {useAuthStore} from '@/stores/authStore';
 import {Outlet} from 'react-router-dom';
-
-// 임시 프로필 사진 url
-const imgUrl =
-  'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg';
+import UserImg from '@/assets/users/user.svg?react';
 
 const SettingLayout = () => {
+  const {user} = useAuthStore();
+
   return (
     <div className='relative flex flex-col'>
       {/* content */}
       <div className='mt-215 w-full bg-[#fff] py-78 px-36 flex flex-col gap-26'>
         <h1 className='text-[#000] text-[32px] font-bold leading-[140%] text-center'>
-          닉네임
+          {user?.nickname}
         </h1>
         <SettingTabBar />
         <Outlet />
       </div>
 
       {/* profile */}
-      <div
-        className='absolute top-215 left-1/2 transform translate-x-[-50%] translate-y-[-50%] rounded-full w-132 h-132 border-1 border-solid border-primary'
-        style={{
-          backgroundImage: `url(${imgUrl})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      />
+      {user?.profileImageUrl ? (
+        <div
+          className='absolute top-215 left-1/2 transform translate-x-[-50%] translate-y-[-50%] rounded-full w-132 h-132 border-1 border-solid border-primary bg-white_1'
+          style={{
+            backgroundImage: `url(${user?.profileImageUrl})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+      ) : (
+        <div className='absolute top-215 left-1/2 transform translate-x-[-50%] translate-y-[-50%] rounded-full w-132 h-132 border-1 border-solid border-primary bg-white_1 overflow-hidden flex justify-center items-center'>
+          <UserImg className='w-100 h-100' />
+        </div>
+      )}
     </div>
   );
 };
