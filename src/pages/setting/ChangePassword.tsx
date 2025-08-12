1827c99a148a690bee6a8904fdaaf209a7f8d601
DandelionQZ <delion.dev@gmail.com>
2025-08-12 12:09:05 +0900

    feat/#91: 비밀번호 변경 페이지에 비밀번호 표시 기능 추가

--- src/pages/setting/ChangePassword.tsx
+++ src/pages/setting/ChangePassword.tsx
@@ -1,7 +1,43 @@
 import BackButtonTitleHeader from '@/components/global/BackButtonTitleHeader';
 import EyeOpen from '@/assets/eye-open.svg?react';
+import {useState} from 'react';
+
+type ChangePasswordType = {
+  value: string;
+  isValid: boolean;
+  isShow: boolean;
+};
+
+interface ChangePasswordState {
+  currentPw: ChangePasswordType;
+  newPw: ChangePasswordType;
+  newPwCheck: ChangePasswordType;
+}
 
 const ChangePassword = () => {
+  const [passwordState, setPasswordState] = useState<ChangePasswordState>({
+    currentPw: {value: '', isValid: false, isShow: false},
+    newPw: {value: '', isValid: false, isShow: false},
+    newPwCheck: {value: '', isValid: false, isShow: false},
+  });
+
+  const handleShowClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const formElement = (e.target as HTMLElement).closest('form');
+    const inputElement = formElement?.querySelector('input');
+    const name = inputElement?.name;
+
+    if (name) {
+      setPasswordState((prev) => ({
+        ...prev,
+        [name]: {
+          ...prev[name as keyof ChangePasswordState],
+          isShow: !prev[name as keyof ChangePasswordState].isShow,
+        },
+      }));
+    }
+  };
+
   return (
     <div>
       {/* header */}
@@ -14,16 +50,21 @@ const ChangePassword = () => {
             현재 비밀번호
           </h1>
           <div className='px-10'>
-            <div className='flex justify-between items-center py-9 px-12 rounded-[5px] border border-solid border-[#000] bg-white text-[#000] gap-10 overflow-hidden'>
+            <form className='flex justify-between items-center py-9 px-12 rounded-[5px] border border-solid border-[#000] bg-white text-[#000] gap-10 overflow-hidden'>
               <input
-                type='text'
+                type={passwordState.currentPw.isShow ? 'text' : 'password'}
+                name='currentPw'
                 minLength={8}
                 maxLength={16}
                 placeholder='기존 비밀번호를 입력해주세요'
                 className='grow placeholder:text-[#cdcdcd] placeholder:text-[18px] leading-24 tracking-[0.18px] focus:outline-0'
               />
-              <EyeOpen className='hover:cursor-pointer shrink-0' />
-            </div>
+              <button type='button' onClick={handleShowClick}>
+                <EyeOpen
+                  className={`hover:cursor-pointer shrink-0 ${passwordState.currentPw.isShow ? 'stroke-[#000]' : 'stroke-gray-2'}`}
+                />
+              </button>
+            </form>
           </div>
         </div>
 
@@ -32,16 +73,21 @@ const ChangePassword = () => {
             새 비밀번호
           </h1>
           <div className='px-10'>
-            <div className='flex justify-between items-center py-9 px-12 rounded-[5px] border border-solid border-[#000] bg-white text-[#000] gap-10 overflow-hidden'>
+            <form className='flex justify-between items-center py-9 px-12 rounded-[5px] border border-solid border-[#000] bg-white text-[#000] gap-10 overflow-hidden'>
               <input
-                type='text'
+                type={passwordState.newPw.isShow ? 'text' : 'password'}
+                name='newPw'
                 minLength={8}
                 maxLength={16}
                 placeholder='새로운 비밀번호를 입력해주세요'
                 className='grow placeholder:text-[#cdcdcd] placeholder:text-[18px] leading-24 tracking-[0.18px] focus:outline-0'
               />
-              <EyeOpen className='hover:cursor-pointer shrink-0' />
-            </div>
+              <button type='button' onClick={handleShowClick}>
+                <EyeOpen
+                  className={`hover:cursor-pointer shrink-0 ${passwordState.newPw.isShow ? 'stroke-[#000]' : 'stroke-gray-2'}`}
+                />
+              </button>
+            </form>
           </div>
         </div>
 
@@ -50,16 +96,21 @@ const ChangePassword = () => {
             새 비밀번호 확인
           </h1>
           <div className='px-10'>
-            <div className='flex justify-between items-center py-9 px-12 rounded-[5px] border border-solid border-[#000] bg-white text-[#000] gap-10 overflow-hidden'>
+            <form className='flex justify-between items-center py-9 px-12 rounded-[5px] border border-solid border-[#000] bg-white text-[#000] gap-10 overflow-hidden'>
               <input
-                type='text'
+                type={passwordState.newPwCheck.isShow ? 'text' : 'password'}
+                name='newPwCheck'
                 minLength={8}
                 maxLength={16}
                 placeholder='새로운 비밀번호를 다시 입력해주세요'
                 className='grow placeholder:text-[#cdcdcd] placeholder:text-[18px] leading-24 tracking-[0.18px] focus:outline-0'
               />
-              <EyeOpen className='hover:cursor-pointer shrink-0' />
-            </div>
+              <button type='button' onClick={handleShowClick}>
+                <EyeOpen
+                  className={`hover:cursor-pointer shrink-0 ${passwordState.newPwCheck.isShow ? 'stroke-[#000]' : 'stroke-gray-2'}`}
+                />
+              </button>
+            </form>
           </div>
         </div>
 
