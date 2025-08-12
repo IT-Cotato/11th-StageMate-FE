daea061ac42177de1db3017ba0f61cad453bb2e6
kimminna <kmina121777@naver.com>
2025-08-12 23:45:08 +0900

    feat/#63: 무한 스크롤을 위한 훅 작성

---
+++ src/hooks/useInfinityQuery.ts
@@ -0,0 +1,15 @@
+import {useInfiniteQuery} from '@tanstack/react-query';
+import {getImagesSimple} from '@/api/searchApi';
+
+export const useInfiniteImageSearch = (query: string, display = 10) => {
+  return useInfiniteQuery({
+    queryKey: ['images', query],
+    queryFn: ({pageParam = 1}) => getImagesSimple(query, display, pageParam),
+    getNextPageParam: (lastPage) => {
+      const nextStart = lastPage.start + lastPage.display;
+      return nextStart <= lastPage.total ? nextStart : undefined;
+    },
+    initialPageParam: 1,
+    enabled: !!query.trim(),
+  });
+};
