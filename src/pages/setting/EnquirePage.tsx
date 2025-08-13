bf46be2244d5f6ac41fa579ab6bf8b186ad72f46
DandelionQZ <delion.dev@gmail.com>
2025-08-14 01:12:33 +0900

    feat/#114: 문의 페이지에 API 요청 및 상태 모달 추가, 카테고리 상태 관리 개선

--- src/pages/setting/EnquirePage.tsx
+++ src/pages/setting/EnquirePage.tsx
@@ -3,26 +3,40 @@ import ChevronRight from '@/assets/arrows/chevron-right.svg?react';
 import {enquiryCategories} from '@/constants/enquiry';
 import {useRef, useState} from 'react';
 import useClickOutside from '@/hooks/useClickOutside';
+import {useMutation} from '@tanstack/react-query';
+import {postInquiries} from '@/api/mypageApi';
+import LoadingOverlay from '@/components/global/LoadingOverlay';
+import Modal from '@/components/global/Modal';
+import {useNavigate} from 'react-router-dom';
+import type {EnquiryCategoryType} from '@/types/enquiry';
 
 const EnquirePage = () => {
+  const navigate = useNavigate();
   const [openDropdown, setOpenDropdown] = useState<boolean>(false);
-  const [category, setCategory] = useState<string>('');
+  const [category, setCategory] = useState<EnquiryCategoryType>({
+    id: '',
+    category: '',
+  });
   const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
   const dropdownRef = useRef<HTMLDivElement>(null);
 
+  const enquireMutation = useMutation({
+    mutationFn: postInquiries,
+  });
+
   const isFormValid = () => {
     const titleValid = title.trim().length >= 3 && title.trim().length <= 100;
-    const categoryValid = category.trim().length > 0;
+    const categoryValid = category.id.trim().length > 0;
     const contentValid =
       content.trim().length >= 3 && content.trim().length <= 5000;
     return titleValid && categoryValid && contentValid;
   };
 
   const handleSubmit = () => {
-    console.log('제목:', title);
-    console.log('카테고리:', category);
-    console.log('문의 내용:', content);
+    const data = {title, category: category.id, content};
+    enquireMutation.mutate(data);
+    console.log(data);
   };
 
   useClickOutside({
@@ -59,7 +73,7 @@ const EnquirePage = () => {
             <div
               className='h-25 border border-solid border-[#000] focus:outline-0 text-[#000] text-xl leading-[110%] flex justify-between items-center overflow-hidden'
               onClick={() => setOpenDropdown((prev) => !prev)}>
-              <p>{category}</p>
+              <p>{category.category}</p>
               <ChevronRight className='rotate-90 w-30 h-30' />
             </div>
             {openDropdown && (
@@ -104,6 +118,23 @@ const EnquirePage = () => {
           </button>
         </div>
       </div>
+
+      {/* 상태 모달 & 오버레이 */}
+      {enquireMutation.isPending && <LoadingOverlay />}
+      {enquireMutation.isSuccess && (
+        <Modal
+          content='성공적으로 문의 메일을 보냈습니다.'
+          rightText='확인'
+          onRightClick={() => navigate(-1)}
+        />
+      )}
+      {enquireMutation.isError && (
+        <Modal
+          content='문의 메일을 발송 중 오류가 발생했습니다.'
+          rightText='확인'
+          onRightClick={() => enquireMutation.reset()}
+        />
+      )}
     </div>
   );
 };
@@ -112,7 +143,7 @@ const CategoryDropdown = ({
   setCategory,
   setOpenDropdown,
 }: {
-  setCategory: (category: string) => void;
+  setCategory: (category: EnquiryCategoryType) => void;
   setOpenDropdown: (open: boolean) => void;
 }) => {
   return (
@@ -122,7 +153,7 @@ const CategoryDropdown = ({
           key={category.id}
           className='hover:cursor-pointer'
           onClick={() => {
-            setCategory(category.id);
+            setCategory(category);
             setOpenDropdown(false);
           }}>
           {category.category}
