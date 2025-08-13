68d8e69990e237691a076e238ab5e4e30c6122aa
DandelionQZ <delion.dev@gmail.com>
2025-08-14 00:49:09 +0900

    fix/#114: 백엔드 api 요청 값에 맞도록 문의 카테고리 id 수정

--- src/constants/enquiry.ts
+++ src/constants/enquiry.ts
@@ -1,26 +1,22 @@
 export const enquiryCategories = [
   {
-    id: 0,
+    id: 'PERFORMANCE_INFO_UPDATE',
     category: '공연 정보 수정 요청',
   },
   {
-    id: 1,
+    id: 'COMMUNITY_USAGE',
     category: '커뮤니티 이용 관련(게시글/댓글/채팅)',
   },
   {
-    id: 2,
-    category: '신고 및 차단 요청',
-  },
-  {
-    id: 3,
+    id: 'ACCOUNT_LOGIN',
     category: '계정/로그인 문제',
   },
   {
-    id: 4,
+    id: 'PARTNERSHIP_AD',
     category: '제휴 및 광고 문의',
   },
   {
-    id: 5,
+    id: 'OTHER(기타)',
     category: '기타',
   },
 ];
