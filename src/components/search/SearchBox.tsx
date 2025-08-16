07e80363f7940a37b87c1c4e617bdbd7eb00489e
kimminna <kmina121777@naver.com>
2025-08-17 01:02:52 +0900

    feat, style/#121: 드롭다운 외곽 클릭 시 닫힘 기능 추가 및 Chevron 애니메이션 추가

--- src/components/search/SearchBox.tsx
+++ src/components/search/SearchBox.tsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import Search from '@/assets/search.svg?react';
 import ChevronDown from '@/assets/chevrons/chevron-down.svg?react';
+import SearchPopularDropdown from './SearchPopularDropdown';
+import useClickOutside from '@/hooks/useClickOutside';
 
 type SearchBoxVariant = 'main' | 'community';
 
@@ -10,10 +12,11 @@ interface SearchBoxProps {
 }
 
 const SearchBox = ({variant = 'main'}: SearchBoxProps) => {
-  const [keyword, setKeyword] = useState('');
+  const [keyword, setKeyword] = useState<string>('');
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
 
   const navigate = useNavigate();
-
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
   const handleSearch = () => {
     const trimmed = keyword.trim();
     if (trimmed) {
@@ -35,12 +38,21 @@ const SearchBox = ({variant = 'main'}: SearchBoxProps) => {
     }
   };
 
+  const handleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+  useClickOutside({
+    ref: dropdownRef,
+    onClickOutside: () => setIsDropdownOpen(false),
+    exclude: (target) =>
+      target.closest('input') !== null || target.closest('svg') !== null,
+  });
+
   const variantStyles = {
     main: {
       wrapper:
         'w-full max-w-full sm:h-[50px] h-40 bg-white flex flex-row items-center rounded-[10px] px-20',
-      input:
-        'w-full flex-1 focus:outline-none sm:text-xl text-[16px] px-20 text-gray',
+      input: 'w-full flex-1 focus:outline-none text-[16px] px-20 text-gray',
     },
     community: {
       wrapper:
@@ -65,7 +77,25 @@ const SearchBox = ({variant = 'main'}: SearchBoxProps) => {
         onKeyDown={handleKeyDown}
         className={variantStyles[variant].input}
       />
-      <ChevronDown className='cursor-pointer text-gray-2 shrink-0 sm:w-25 w-20' />
+      <ChevronDown
+        className={`cursor-pointer text-gray-2 shrink-0 sm:w-25 w-20 transition-transform duration-300 ${
+          isDropdownOpen ? 'rotate-180' : 'rotate-0'
+        }`}
+        onClick={handleDropdown}
+      />
+
+      <div
+        className={`absolute top-[calc(100%-15px)] right-0 transition-all duration-300 ease-in-out overflow-visible ${
+          isDropdownOpen
+            ? 'opacity-100 translate-y-0 max-h-96'
+            : 'opacity-0 -translate-y-2 max-h-0'
+        }`}>
+        <div
+          className='bg-white rounded-b-[10px] w-280 shadow-xs'
+          ref={dropdownRef}>
+          <SearchPopularDropdown isDropdownOpen />
+        </div>
+      </div>
     </div>
   );
 };
