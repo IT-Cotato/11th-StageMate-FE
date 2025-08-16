dac765f21836cd085081077c931749ce1f3d74e8
kimminna <kmina121777@naver.com>
2025-08-17 01:04:36 +0900

    style/#121: 로고, 빠른 검색창 헤더 고정되게 수정

--- src/components/search/SearchHeader.tsx
+++ src/components/search/SearchHeader.tsx
@@ -1,10 +1,11 @@
 import Logo from '@/assets/logos/main-logo.svg?react';
-import Bell from '@/assets/alerts-feedback/bell.svg?react';
-import User from '@/assets/users/user.svg?react';
 import {useEffect, useState} from 'react';
+import ChevronLeft from '@/assets/chevrons/chevron-left.svg?react';
+import {useNavigate} from 'react-router-dom';
 
 const SearchHeader = () => {
   const [hasBorder, setHasBorder] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,15 +18,21 @@ const SearchHeader = () => {
 
   return (
     <div
-      className={`fixed top-0 max-w-[600px] w-full h-[65px] bg-white z-50 flex flex-row items-center justify-between px-20 transition-shadow duration-300 ${hasBorder ? 'shadow-2xs bg-white/95' : ''}`}>
+      className={`fixed top-0 max-w-[600px] w-full h-[120px] bg-white z-50 flex items-center justify-between px-20 transition-shadow duration-300 ${hasBorder ? 'shadow-2xs bg-white/95' : ''}`}>
       <div
         className={`absolute bottom-0 left-0 w-full bg-gray-200 transition-opacity duration-300 ${hasBorder ? 'opacity-100' : 'opacity-0'}`}
       />
-      <Logo className='sm:w-150 w-120' />
-
-      <div className='flex flex-row gap-18'>
-        <Bell className='cursor-pointer sm:w-30 sm:h-30 w-25 h-25' />
-        <User className='cursor-pointer sm:w-30 sm:h-30 w-25 h-25' />
+      <div className='flex flex-col gap-10'>
+        <Logo className='sm:w-150 w-120' />
+        <div className='flex flex-row items-center text-center '>
+          <ChevronLeft
+            className='w-40 h-40 text-gray cursor-pointer'
+            onClick={() => navigate(-1)}
+          />
+          <span className='absolute left-1/2 -translate-x-1/2 text-xl '>
+            빠른 검색창
+          </span>
+        </div>
       </div>
     </div>
   );
