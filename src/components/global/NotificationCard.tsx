15bb09d54e123efef6726b02c0273a211beef303
kimminna <kmina121777@naver.com>
2025-08-17 01:34:40 +0900

    feat/#127: 알림 API 연결

--- src/components/global/NotificationCard.tsx
+++ src/components/global/NotificationCard.tsx
@@ -1,31 +1,32 @@
-import formatKoreanTime from '@/util/formatKoreanTime';
 import Comment from '@/assets/notification/comment.svg?react';
+import {useNavigate} from 'react-router-dom';
 /**
  * 알림 카드 컴포넌트
  * 사용자가 작성한 커뮤니티 글의 댓글에 대한 알림
  * 사용자가 작성한 댓글의 답글에 대한 알림
  */
 interface NotificationProps {
-  postTitle: string;
-  createdAt: string;
-  type: string;
+  title: string;
+  formattedDate: string;
+  content: string;
+  postId: number;
 }
-const NotificationCard = ({postTitle, createdAt, type}: NotificationProps) => {
-  const message =
-    type === 'COMMENT_ON_MY_POST'
-      ? '누군가 댓글을 남겼어요'
-      : '댓글에 누군가 답장을 했어요';
-  const formattedDate = formatKoreanTime(createdAt);
-
+const NotificationCard = ({
+  title,
+  formattedDate,
+  content,
+  postId,
+}: NotificationProps) => {
+  const navigate = useNavigate();
   return (
-    <div className='w-[466px] flex items-center justify-between bg-[#DDE1E6]/75 rounded-[20px] py-10 px-20 gap-20 cursor-pointer h-62'>
+    <div
+      className='w-[466px] flex items-center justify-between bg-[#DDE1E6]/75 rounded-[20px] py-10 px-20 gap-20 cursor-pointer h-62'
+      onClick={() => navigate(`/community/${postId}`)}>
       <div className='flex flex-row gap-20 items-center flex-1 min-w-0'>
         <Comment className='w-27' />
         <div className='flex flex-col min-w-0'>
-          <span className='font-semibold text-[18px] truncate'>
-            {postTitle}
-          </span>
-          <span className='text-[12px]'>{message}</span>
+          <span className='font-semibold text-[18px] truncate'>{title}</span>
+          <span className='text-[12px]'>{content}</span>
         </div>
       </div>
 
