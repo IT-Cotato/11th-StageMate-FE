567d3a022a09c14736f37899ace98e815973acaf
kimminna <kmina121777@naver.com>
2025-08-17 01:06:53 +0900

    feat/#121: 이미지 검색 기능 API 연결

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,4 @@
 import SearchHeader from '@/components/search/SearchHeader';
-import ChevronLeft from '@/assets/chevrons/chevron-left.svg?react';
 import Search from '@/assets/search.svg?react';
 import {useNavigate, useSearchParams} from 'react-router-dom';
 import CustomDatePicker from '@/components/global/CustomDatePicker';
@@ -8,10 +7,12 @@ import ChevronDown from '@/assets/chevrons/chevron-down.svg?react';
 import {motion, AnimatePresence} from 'framer-motion';
 import {format} from 'date-fns';
 import {ko} from 'date-fns/locale';
-import {useRef, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import SelectGenreModal from '@/components/modal/SelectGenreModal';
 import useClickOutside from '@/hooks/useClickOutside';
 import SearchResults from '@/components/search/SearchResults';
+import {useSearchResult} from '@/hooks/useSearchResults';
+import {performanceGenreSearchMap} from '@/util/performanceCategory';
 
 const SearchPage = () => {
   const [params] = useSearchParams();
@@ -35,29 +36,40 @@ const SearchPage = () => {
   });
 
   const toggleGenre = (genre: string) => {
-    setSelectedGenre((prev) =>
-      prev.includes(genre) ? prev.filter((g) => g !== genre) : [...prev, genre]
-    );
+    setSelectedGenre([genre]);
   };
 
   const handleToggleGenre = (e: React.MouseEvent) => {
     e.stopPropagation();
     setIsGenreOpen((prev) => !prev);
   };
+  const {data, isLoading, isError, refetch} = useSearchResult({
+    keyword: submittedKeyword,
+    performanceGenre: selectedGenre[0]
+      ? performanceGenreSearchMap[selectedGenre[0]]
+      : undefined,
+    date: startDate ? format(startDate, 'yyyy-MM-dd') : undefined,
+  });
+  const handleSearch = () => {
+    if (!searchKeyword.trim()) return;
+    const params = new URLSearchParams();
+    params.set('keyword', searchKeyword.trim());
+    navigate(`/search?${params.toString()}`);
+    setSubmittedKeyword(searchKeyword.trim());
+    refetch();
+  };
+  useEffect(() => {
+    if (keywordFromQuery) {
+      setSearchKeyword(keywordFromQuery);
+      setSubmittedKeyword(keywordFromQuery);
+      refetch();
+    }
+  }, [keywordFromQuery, refetch]);
 
   return (
     <div className='overflow-x-hidden flex justify-center w-full min-h-screen'>
       <SearchHeader />
-      <div className='pt-90 w-full max-w-[600px] px-20 flex flex-col gap-30'>
-        {/* 상단 뒤로가기 */}
-        <div className='flex flex-row items-center text-center '>
-          <ChevronLeft
-            className='w-40 h-40 text-gray cursor-pointer'
-            onClick={() => navigate(-1)}
-          />
-          <span className='w-full mr-30 text-xl'>빠른 검색창</span>
-        </div>
-
+      <div className='pt-120 w-full max-w-[600px] px-20 flex flex-col gap-30'>
         {/* 검색 입력 */}
         <span className='text-xl ml-20 font-semibold'>빠른 검색</span>
         <div className='bg-gray-1 rounded-[20px] w-full h-60 flex flex-row px-20 items-center'>
@@ -69,19 +81,20 @@ const SearchPage = () => {
             onChange={(e) => setSearchKeyword(e.target.value)}
             onKeyDown={(e) => {
               if (e.key === 'Enter') {
-                setSubmittedKeyword(searchKeyword.trim());
+                if (e.key === 'Enter') handleSearch();
               }
             }}
           />
-          <Search
-            onClick={() => setSubmittedKeyword(searchKeyword.trim())}
-            className='cursor-pointer'
-          />
+          <Search onClick={handleSearch} className='cursor-pointer' />
         </div>
 
         {/* 검색어 입력 여부에 따라 조건 렌더링 */}
         {submittedKeyword ? (
-          <SearchResults />
+          <SearchResults
+            data={data?.data}
+            isLoading={isLoading}
+            isError={isError}
+          />
         ) : (
           <>
             {/* 날짜 선택 */}
@@ -109,13 +122,17 @@ const SearchPage = () => {
               <span className='text-xl ml-20 font-semibold'>장르 선택</span>
               <div className='h-60 bg-[#fff] rounded-[20px] flex items-center px-20 justify-between border-gray-1 border-[1px]'>
                 <span
-                  className={`text-base ${selectedGenre.length > 0 ? 'text-black' : 'text-gray-400'}`}>
-                  {selectedGenre.length > 0
-                    ? selectedGenre.map((g) => `#${g}`).join(', ')
+                  className={`text-base ${
+                    selectedGenre.length > 0 && selectedGenre[0]
+                      ? 'text-black'
+                      : 'text-gray-2'
+                  }`}>
+                  {selectedGenre.length > 0 && selectedGenre[0]
+                    ? `# ${selectedGenre[0]}`
                     : '장르를 선택해 주세요.'}
                 </span>
                 <ChevronDown
-                  className={`chevron-toggle transition-transform duration-300 cursor-pointer ${
+                  className={`chevron-toggle transition-transform duration-300 cursor-pointer text-gray-2 ${
                     isGenreOpen ? 'rotate-180' : ''
                   }`}
                   onClick={handleToggleGenre}
@@ -140,7 +157,6 @@ const SearchPage = () => {
                       dragConstraints={{top: 0, bottom: 100}}
                       onDragEnd={(_, info) => {
                         if (info.point.y > 300) setIsGenreOpen(false);
-                        console.log(event);
                       }}>
                       <SelectGenreModal
                         selectedGenre={selectedGenre}
