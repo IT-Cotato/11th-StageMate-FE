99bde37c1d5958f88edb2c35cbb27b8c1aa012c8
DandelionQZ <delion.dev@gmail.com>
2025-08-18 11:52:57 +0900

    feat/#146: 채팅방 목록 API 연동 및 페이지네이션 추가

--- src/components/community/chat/ChatRoomItem.tsx
+++ src/components/community/chat/ChatRoomItem.tsx
@@ -1,9 +1,9 @@
 import Wifi from '@/assets/community/live-chat-wifi.svg?react';
 import Door from '@/assets/community/live-chat-door.svg?react';
-import type {ChatRoom} from '@/types/community';
+import type {ChatRoomType} from '@/types/chat';
 
 interface ChatRoomItemProps {
-  room: ChatRoom;
+  room: ChatRoomType;
   onClick?: () => void;
 }
 const ChatRoomItem = ({room, onClick}: ChatRoomItemProps) => {
@@ -13,7 +13,7 @@ const ChatRoomItem = ({room, onClick}: ChatRoomItemProps) => {
       <div className='flex flex-row flex-1 justify-between cursor-pointer'>
         <div>{room.title}</div>
         <div>
-          오픈 시간: {room.startTime} ~ {room.endTime}
+          오픈 시간: {room.startDate} ~ {room.endDate}
         </div>
       </div>
       <Door className='w-20 ml-11 cursor-pointer' onClick={onClick} />
--- src/pages/community/ChatPage.tsx
+++ src/pages/community/ChatPage.tsx
@@ -1,10 +1,36 @@
 import ChatRoomItem from '@/components/community/chat/ChatRoomItem';
 import useCommunityNavigation from '@/hooks/useCommunityNavigation';
-import {mockChatRooms} from '@/mocks/mockChatRooms';
 import Search from '@/assets/search.svg?react';
+import Pagination from 'react-js-pagination';
+import {useQuery} from '@tanstack/react-query';
+import {useState} from 'react';
+import {getChatRoom} from '@/api/chatApi';
+import type {ChatRoomType} from '@/types/chat';
 
 const ChatPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const {goToChatRoomDetail} = useCommunityNavigation();
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const {data, isLoading, error} = useQuery({
+    queryKey: ['chatRoomList', currentPage],
+    queryFn: () => getChatRoom({page: currentPage - 1}),
+  });
+
+  if (isLoading) {
+    return <div>채팅방 불러오는 중 ...</div>;
+  }
+
+  if (error) {
+    return <div>채팅방을 불러오는 데 실패했습니다.</div>;
+  }
+
+  const ChatRoomList = data?.list || [];
+  const ITEMS_PER_PAGE = data.pageSize;
+  const totalItemsCount = data.totalElements;
 
   return (
     <div className='px-16'>
@@ -21,13 +47,31 @@ const ChatPage = () => {
 
       {/* 채팅방 목록 */}
       <div className='flex flex-col gap-10'>
-        {mockChatRooms.map((room) => (
+        {ChatRoomList.map((room: ChatRoomType) => (
           <ChatRoomItem
-            key={room.id}
+            key={room.chatRoomId}
             room={room}
-            onClick={() => goToChatRoomDetail(room.id)}
+            onClick={() => goToChatRoomDetail(room.chatRoomId)}
           />
         ))}
+      </div>
+
+      {/* 페이지네이션 */}
+      <div className='flex justify-center'>
+        <Pagination
+          activePage={currentPage}
+          itemsCountPerPage={ITEMS_PER_PAGE}
+          totalItemsCount={totalItemsCount}
+          pageRangeDisplayed={5}
+          onChange={handlePageChange}
+          innerClass='flex gap-6'
+          itemClass='px-5 py-1 text-sm'
+          activeClass='font-bold'
+          prevPageText='<'
+          nextPageText='>'
+          firstPageText='<<'
+          lastPageText='>>'
+        />
       </div>
     </div>
   );
