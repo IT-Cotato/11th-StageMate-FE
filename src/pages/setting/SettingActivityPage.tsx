c4a323ff7cc6eaea94f0c0a72c506958f3cb62ac
kimminna <kmina121777@naver.com>
2025-08-20 21:37:25 +0900

    feat/#162: 내 활동 - 네비게이팅 경로 연결

--- src/pages/setting/SettingActivityPage.tsx
+++ src/pages/setting/SettingActivityPage.tsx
@@ -1,32 +1,44 @@
 import ChevronRight from '@/assets/arrows/chevron-right.svg?react';
+import {useNavigate} from 'react-router-dom';
 
 const SettingActivityPage = () => {
+  const navigate = useNavigate();
   return (
     <div className='flex flex-col gap-29'>
       {/* info */}
       <div className='mx-28 py-37 px-33 flex flex-col bg-[#d9d9d9] rounded-[10px]'>
-        <div className='flex justify-between items-center hover:cursor-pointer'>
+        <div
+          className='flex justify-between items-center hover:cursor-pointer'
+          onClick={() => navigate('/written-post')}>
           <p className='text-xl font-semibold leading-[140%]'>
             내가 작성한 글 모아보기
           </p>
           <ChevronRight className='w-30 h-30' />
         </div>
         <div className='h-1 bg-[#000] my-12' />
-        <div className='flex justify-between items-center hover:cursor-pointer'>
+        <div
+          className='flex justify-between items-center hover:cursor-pointer'
+          onClick={() => {
+            navigate('/written-comment');
+          }}>
           <p className='text-xl font-semibold leading-[140%]'>
             댓글 단 글 모아보기
           </p>
           <ChevronRight className='w-30 h-30' />
         </div>
         <div className='h-1 bg-[#000] my-12' />
-        <div className='flex justify-between items-center hover:cursor-pointer'>
+        <div
+          className='flex justify-between items-center hover:cursor-pointer'
+          onClick={() => navigate('/scrap-post')}>
           <p className='text-xl font-semibold leading-[140%]'>
             스크랩한 글 모아보기
           </p>
           <ChevronRight className='w-30 h-30' />
         </div>
         <div className='h-1 bg-[#000] my-12' />
-        <div className='flex justify-between items-center hover:cursor-pointer'>
+        <div
+          className='flex justify-between items-center hover:cursor-pointer'
+          onClick={() => navigate('/written-magazine')}>
           <p className='text-xl font-semibold leading-[140%]'>
             스크랩한 매거진 모아보기
           </p>
