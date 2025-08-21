17059403b3863ff00b21f15deb21445084e472eb
DandelionQZ <delion.dev@gmail.com>
2025-08-21 14:48:59 +0900

    feat/#174: 구글 로그인 버튼 클릭 핸들러 추가 및 클릭 이벤트 수정

--- src/components/auth/AuthBrandBadges.tsx
+++ src/components/auth/AuthBrandBadges.tsx
@@ -2,8 +2,13 @@ import FacebookLight from '@/assets/brands/facebook-light.svg?react';
 import GoogleLight from '@/assets/brands/google-light.svg?react';
 import TwitterLight from '@/assets/brands/twitter-light.svg?react';
 import AppleLight from '@/assets/brands/apple-light.svg?react';
+import {BASE_URL} from '@/api/urls';
 
 const AuthBrandBadges = () => {
+  const handleGoogleLogin = () => {
+    window.location.href = `${BASE_URL}/login/oauth2/authorization/google`;
+  };
+
   return (
     <div className='flex items-center gap-20'>
       <button
@@ -12,7 +17,7 @@ const AuthBrandBadges = () => {
         <FacebookLight />
       </button>
       <button
-        onClick={() => console.log('google 버튼 클릭')}
+        onClick={handleGoogleLogin}
         className='flex w-50 h-50 p-10 items-center justify-center gap-10 rounded-[25px] bg-gray-1'>
         <GoogleLight />
       </button>
